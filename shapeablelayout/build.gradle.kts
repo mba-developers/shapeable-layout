e95124d0ae6892b7de4c3774780bfd72afe1c430
Balavignesh <vigneshpillai153@gmail.com>
2024-11-10 18:41:44 +0530

    - library setup

--- shapeablelayout/build.gradle.kts
+++ shapeablelayout/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.android.build.gradle.internal.utils.createPublishingInfoForApp
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -40,4 +43,33 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+
+afterEvaluate {
+    // Configure the publishing block after evaluation
+    (extensions.getByName("publishing") as PublishingExtension).apply {
+        publications {
+            create<MavenPublication>("release") {
+                // Configure the publication to use the release component
+                from(components["release"])
+
+                groupId = "com.github.mba-developers" // Your group ID
+                artifactId = "shapeable-layout" // Your artifact ID
+                version = "1.0" // Your version
+            }
+        }
+
+        repositories {
+            maven {
+                // Publish to a local Maven repository
+                url = uri("${project.rootDir}/maven-repo")
+                // Uncomment if publishing to a remote repository:
+                // credentials {
+                //     username = project.findProperty("mavenUser") as String? ?: System.getenv("MAVEN_USER")
+                //     password = project.findProperty("mavenPassword") as String? ?: System.getenv("MAVEN_PASSWORD")
+                // }
+            }
+        }
+    }
 }

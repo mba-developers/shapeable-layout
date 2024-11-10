f38613cfc6b40fd1b11649e6367bfa11068a9a35
Balavignesh <vigneshpillai153@gmail.com>
2024-11-10 19:36:27 +0530

    - library setup changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.mbadevelopers.shapeablelayout"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mbadevelopers.shapeablelayout"
--- shapeablelayout/build.gradle.kts
+++ shapeablelayout/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.mbadevelopers.shapeablelayout"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 24
